bc9e9249de97481a3bf437457b8bb3cd502befde
Clazariuk1 <Clazariuk@gmail.com>
2023-12-16 18:07:39 -0800

    Tip Calculator express application created

--- hw-part-one/server.js
+++ hw-part-one/server.js
@@ -42,6 +42,18 @@ app.get('/greeting/:name', (req, res) => {
     res.send(`Hey... Isn't your name ${req.params.name}?`)
 })
 
+app.get('/tip', (req, res) => {
+    res.send(`I'm bad at math, what's a good tip for the bill?`)
+})
+
+app.get('/tip/:total/:percentage', (req, res) => {
+    let tipConversion = `${req.params.percentage}`/ 100
+    let tipValue =`${req.params.total}` * `${tipConversion}`
+    let grandTotal = `${req.params.total}`
+    grandTotal = parseInt(`${grandTotal}`) + parseInt(`${tipValue}`)
+    res.send(`So your bill was ${req.params.total} and you want to tip ${req.params.percentage} percent? That's gonna be another ${tipValue}, bringing your grand total up to ${grandTotal} dollars.`)
+})
+
 // routes end
 app.listen(3000, () => {
     console.log("We're live.")
